1f61238016bcf7a00e4f55051ed6493aed5addb5
itsmeezi <124519394+itsmeezi@users.noreply.github.com>
2023-12-10 00:08:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click",()=> {question.innerHTML = "Tadaaaaa,Ganteng Bett kann";
+gif.src =
+    "https://github.com/itsmeezi/its.git.hub.io/blob/main/WhatsApp%20Image%202023-12-09%20at%2023.48.24.jpeg?raw=true";});
+
+    noBtn.addEventListener("mouseover"), ()=>{
+        const noBtnRect = noBtn.getBoundingClientRect();
+        const maxX = window.innerWidth - noBtnRect.width;
+        const MaxY = window.innerHeight - noBtnRect.height;
+
+        const randomX = Math.floor(Math.random() * MaxX);
+        const randomY = Math.floor(Math.random() * MaxY);
+
+        noBtn.style.left = randomX + "px";
+        noBtn.style.top = randomY +"px"
+   }
